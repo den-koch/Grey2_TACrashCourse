0e3d3c1e7f0d22a3e4ade9a33bd66a14b1b9e334
Ivan Holotsvan <iholotsvan@gmail.com>
2023-11-09 17:02:54 +0200

    Add tests.

---
+++ src/com/softserve/Collections/Homework/Task1/MainTest.java
@@ -0,0 +1,147 @@
+package com.softserve.Collections.Homework.Task1;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.softserve.Collections.Homework.Task1.Main.*;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testUnionWithEmptySets() {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        Set<Integer> result = union(set1, set2);
+
+        Set<Integer> expected = new HashSet<>();
+
+        assertEquals("Union of two empty sets should be an empty set", expected, result);
+    }
+
+    @Test
+    public void testUnionIntegerSets() {
+        Set<Integer> set1 = Set.of(1, 2, 3, 4);
+        Set<Integer> set2 = Set.of(3, 4, 5, 6);
+
+        Set<Integer> result = union(set1, set2);
+
+        Set<Integer> expected = Set.of(1, 2, 3, 4, 5, 6);
+
+        assertEquals("Union of integer sets should have all unique integers", expected, result);
+    }
+
+
+    @Test
+    public void testUnionDoubleSets() {
+        Set<Double> set1 = Set.of(1.5, 2.0, 3.5);
+        Set<Double> set2 = Set.of(3.0, 4.5, 5.0);
+
+        Set<Double> result = union(set1, set2);
+
+        Set<Double> expected = Set.of(1.5, 2.0, 3.5, 3.0, 4.5, 5.0);
+
+        assertEquals("Union of double sets should have all unique double values", expected, result);
+    }
+
+    @Test
+    public void testUnionCharSets() {
+        Set<Character> set1 = Set.of('a', 'b', 'c');
+        Set<Character> set2 = Set.of('c', 'd', 'e', 'a');
+
+        Set<Character> result = union(set1, set2);
+
+        Set<Character> expected = Set.of('a', 'b', 'c', 'd', 'e');
+
+        assertEquals("Union of character sets should have all unique characters", expected, result);
+    }
+
+    @Test
+    public void testUnionStringSets() {
+        Set<String> set1 = Set.of("apple", "banana", "cherry", "grape");
+        Set<String> set2 = Set.of("cherry", "banana", "date", "fig", "grape");
+
+        Set<String> result = union(set1, set2);
+
+        Set<String> expected = Set.of("apple", "banana", "cherry", "date", "fig", "grape");
+
+        assertEquals("Union of string sets should have all unique strings", expected, result);
+    }
+
+    @Test
+    public void testUnionMixedDataTypes() {
+        Set<Object> set1 = Set.of(1, 2.0, 'a', 'b');
+        Set<Object> set2 = Set.of('b', "apple", 2.0);
+
+        Set<Object> result = union(set1, set2);
+
+        Set<Object> expected = Set.of(1, 2.0, 'a', 'b', "apple");
+
+        assertEquals("Union of sets with mixed data types should preserve data types", expected, result);
+    }
+
+    @Test
+    public void testUnionEmpSets() {
+        Set<Main.Emp> set1 = Set.of(
+                new Emp(2, "Bob"),
+                new Emp(4, "Dave"),
+                new Emp(5, "Eve")
+        );
+        Set<Main.Emp> set2 = Set.of(
+                new Emp(1, "Bob"),
+                new Emp(4, "Dave"),
+                new Emp(5, "Eve"),
+                new Emp(6, "Mark")
+        );
+        Set<Emp> result = union(set1, set2);
+        Set<Emp> expected = Set.of(
+                new Emp(2, "Bob"),
+                new Emp(4, "Dave"),
+                new Emp(5, "Eve"),
+                new Emp(1, "Bob"),
+                new Emp(6, "Mark")
+        );
+
+        assertEquals("Union of sets with mixed data types should have all unique employees", expected, result);
+    }
+
+    @Test
+    public void testIntersectWithEmptySets() {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        Set<Integer> result = intersect(set1, set2);
+
+        Set<Integer> expected = new HashSet<>();
+
+        assertEquals("Intersect of two empty sets should be an empty set", expected, result);
+    }
+
+    @Test
+    public void testIntersectCommonElements() {
+        Set<Integer> set1 = Set.of(1, 2, 3, 4);
+        Set<Integer> set2 = Set.of(3, 4, 5, 6);
+
+        Set<Integer> result = intersect(set1, set2);
+
+        Set<Integer> expected = Set.of(3, 4);
+
+        assertEquals("Intersection of sets with common elements should have common elements", expected, result);
+    }
+
+    @Test
+    public void testIntersectNoCommonElements() {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(4, 5, 6);
+
+        Set<Integer> result = intersect(set1, set2);
+        Set<Integer> expected = Set.of();
+
+        assertEquals("Intersection of sets with no common elements should be an empty set", expected, result);
+    }
+}
---
+++ src/com/softserve/Collections/Homework/Task2/MainTest.java
@@ -0,0 +1,75 @@
+package com.softserve.Collections.Homework.Task2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.softserve.Collections.Homework.Task2.Main.isDuplicatedFirstName;
+import static com.softserve.Collections.Homework.Task2.Main.removePersonsWithFirstName;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    public void testIsDuplicatedFirstNameWithDuplicates() {
+        Map<String, String> pMap = new HashMap<>();
+        pMap.put("Holotsvan", "Ivan");
+        pMap.put("Doe", "Jhon");
+        pMap.put("Mazepa", "Ivan");
+        pMap.put("Shakespeare", "William");
+
+        boolean result = isDuplicatedFirstName(pMap);
+
+        assertTrue(result, "There are duplicated first names, so the result should be true.");
+    }
+
+    @Test
+    public void testIsDuplicatedFirstNameWithoutDuplicates() {
+        Map<String, String> pMap = new HashMap<>();
+        pMap.put("Tkach", "Stas");
+        pMap.put("Doe", "Jhon");
+        pMap.put("Ivan", "Holotsvan");
+
+        boolean result = isDuplicatedFirstName(pMap);
+
+        assertFalse(result, "There are no duplicated first names, so the result should be false.");
+    }
+
+    @Test
+    public void testIsDuplicatedFirstNameWithEmptyMap() {
+        Map<String, String> pMap = new HashMap<>();
+
+        boolean result = isDuplicatedFirstName(pMap);
+
+        assertFalse(result, "The map is empty, so there are no duplicated first names, and the result should be false.");
+    }
+
+    @Test
+    public void testRemovePersonsWithFirstName() {
+        Map<String, String> pMap = new HashMap<>();
+        pMap.put("Ivanenko", "Ivan");
+        pMap.put("Petrenko", "Olena");
+        pMap.put("Kovalenko", "Olena");
+        pMap.put("Sydorenko", "Ivan");
+
+        removePersonsWithFirstName(pMap, "Olena");
+
+        assertFalse(pMap.containsValue("Olena"), "Entries with first name 'Olena' should be removed.");
+    }
+
+    @Test
+    public void testRemovePersonsWithFirstNameNoMatch() {
+        Map<String, String> pMap = new HashMap<>();
+        pMap.put("Ivanov", "Petro");
+        pMap.put("Petrov", "Oksana");
+        pMap.put("Koval", "Vasyl");
+        pMap.put("Sydorov", "Mariya");
+        int expectedSize = pMap.size();
+
+        removePersonsWithFirstName(pMap, "Yuriy");
+        int resultedSize = pMap.size();
+        Assertions.assertEquals(expectedSize, resultedSize, "Hash map shouldn't be edited.");
+    }
+}
